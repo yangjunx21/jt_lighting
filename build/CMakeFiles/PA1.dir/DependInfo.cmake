8db94632a8ba01af8e7e43767b26dad3ea20c124
yjx <yangjunx21@mails.tsinghua.edu.cn>
2023-06-21 20:43:02 +0800

    111

---
+++ build/CMakeFiles/PA1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/src/image.cpp" "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/build/CMakeFiles/PA1.dir/src/image.cpp.o"
+  "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/src/main.cpp" "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/build/CMakeFiles/PA1.dir/src/main.cpp.o"
+  "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/src/mesh.cpp" "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/build/CMakeFiles/PA1.dir/src/mesh.cpp.o"
+  "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/src/scene_parser.cpp" "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/build/CMakeFiles/PA1.dir/src/scene_parser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../deps/vecmath/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yangjunx/basic_grapgical/hw/code/jt_lighting/build/deps/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PA1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA1.dir/src/image.cpp.o"
+  "CMakeFiles/PA1.dir/src/main.cpp.o"
+  "CMakeFiles/PA1.dir/src/mesh.cpp.o"
+  "CMakeFiles/PA1.dir/src/scene_parser.cpp.o"
+  "PA1"
+  "PA1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
